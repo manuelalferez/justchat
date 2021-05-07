65d34bf7eb7d7fbdd38f71864a60ef7fa7e1f2d2
manuelalferez <manuelalferezruiz@gmail.com>
2021-05-07 17:49:27 +0300

    basic main page with login frontend

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Index = () => {
-	return <div className="text-xl grid justify-items-center">JustChat</div>;
+	return (
+		<div className="grid justify-items-center bg-green-100 h-screen">
+			<Link href="/">
+				<a className="h-4">
+					<Image src="/logo.png" alt="JustChat logo" width={200} height={100} />
+				</a>
+			</Link>
+			<form method="POST" className="flex flex-col w-96 justify-center">
+				<input
+					type="text"
+					id="user"
+					placeholder="User"
+					name="user"
+					className="mb-2 p-3 pl-7 rounded-sm shadow-md"
+				/>
+				<input
+					type="password"
+					id="password"
+					placeholder="Password"
+					name="password"
+					className="mb-6 p-3 pl-7 rounded-sm shadow-md"
+				/>
+				<button
+					type="submit"
+					className="hover:bg-green-800 bg-green-600 shadow-md text-white rounded-md py-2 w-1/2 self-center"
+				>
+					Login
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Index;
